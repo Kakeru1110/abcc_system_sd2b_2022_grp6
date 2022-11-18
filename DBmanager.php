4a049bd07b3dcb9ad203ab2b5eee9a312ac324cb
odanihayato <114891191+odanihayato@users.noreply.github.com>
2022-11-18 10:54:03 +0900

    Create DBmanager.php

---
+++ DBmanager.php
@@ -0,0 +1,22 @@
+<?php
+class DBManager {
+	//接続のメソッド
+	private function dbConnect(){
+		$pdo = new PDO('mysql:host=localhost;dbname=webdb;charset=utf8',
+							'webuser', 'abccsd2');
+		return $pdo;
+	}
+
+public function DBtouroku($userID, $userpass){
+		$pdo = $this->dbConnect();
+
+		$sql = "INSERT INTO user_tbl("仮")(userID,userpass)VALUES(?,?)";
+		$ps = $pdo->prepare($sql);
+		$ps->bindValue(1,$_POST['userID'],PDO::PARAM_STR);
+		$ps->bindValue(2,$_POST['userpass'],PDO::PARAM_STR);
+		$ps->execute();
+
+	}
+}
+
+public function 
