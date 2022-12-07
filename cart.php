82175c758d215f864fba934a385972574620f94f
Kakeru1011 <2101167@s.asojuku.ac.jp>
2022-12-07 13:31:25 +0900

    Update cart.php

--- cart.php
+++ cart.php
@@ -2,6 +2,7 @@
     session_start();
     require_once 'DBmanager.php';
     $cls = new DBManager();
+    $cls->userSessionCheck();
     $user_id = $_SESSION['user_id'];
     $cart = $cls->getCartTblByUserid($user_id);
     $cntitem = 0;
@@ -11,23 +12,57 @@
         header('Location: ./cart.php');
         exit();
     }
+    foreach($cart as $row){
+        $id = $row['cart_id'];
+        if(isset($_POST["$id"])){
+            $cls->DeleteCartTblByCartid($id);
+            header('Location: ./cart.php');
+            exit();
+        }
+    }
 ?>
 
 <!DOCTYPE html>
 <html>
     <head>
         <link href="css/cart.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>カート</title>
     </head>
-    <body>
+    <body style="background:#e4edfc;">
+    <nav class="navbar navbar-dark bg-dark mb-4" aria-label="First navbar example">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"><img src="image/logo_touka2.png" style="width:200px; height: 40px;"></a>
+              <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+        
+              <div class="navbar-collapse collapse" id="navbarsExample01">
+                <ul class="navbar-nav me-auto mb-2">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="item.php">ホーム</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="cart.php">カート</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="loginPage.php">ログアウト</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
+    <div class="cart">
     <?php
         
         if(isset($cart)){
             $_SESSION['cart'] = $cart;
             foreach($cart as $row){
+                $id = $row['cart_id'];
                 $cart_item = $cls->getItemsTblByItem_id($row['item_id']);
                 foreach($cart_item as $data){
-                    echo $data['item_name'].$data['item_price']."<br>";
+                    echo '<ul　class="list_design027"><li><span>'.$data['item_name'].'　'.$data['item_price'].'円 <form action="" method="POST"><input type="submit" value="カートから削除" name="'.$id.'"></form></span></li></ul>';
                     $cntitem++;
                     $sumprice += $data['item_price'];
                 }
@@ -42,6 +77,8 @@
             <a class="btn" id="kounyu-btn" href="buyCheck.php?cntitem='.$cntitem.'&sumprice='.$sumprice.'">購入</a><br>
         </form>';
         ?>     
-        <button class="btn" id="back-btn">商品一覧に戻る</button>
+        <button class="btn" id="back-btn" onclick="location.href='item.php'">商品一覧に戻る</button>
+    </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
