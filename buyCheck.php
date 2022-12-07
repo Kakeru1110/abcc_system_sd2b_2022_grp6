477aa859ed8d1d89b9f88bb7ee66daee31ca9cfb
Kakeru1011 <2101167@s.asojuku.ac.jp>
2022-12-07 13:31:18 +0900

    Update buyCheck.php

--- buyCheck.php
+++ buyCheck.php
@@ -9,8 +9,32 @@
     <head>
         <title>購入チェック</title>
         <link href="css/kounyucheck.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
-    <body>
+    <body style="background:#e4edfc;">
+    <nav class="navbar navbar-dark bg-dark mb-4" aria-label="First navbar example">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#"><img src="image/logo_touka2.png" style="width:200px; height: 40px;"></a>
+              <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample01" aria-controls="navbarsExample01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+        
+              <div class="navbar-collapse collapse" id="navbarsExample01">
+                <ul class="navbar-nav me-auto mb-2">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="item.php">ホーム</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="cart.php">カート</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="loginPage.php">ログアウト</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+        </nav>
         <?php
         if(isset($_POST['itembuy'])){
             $item = $cls->getItemsTblByItem_id($_POST['itembuy']);
@@ -31,5 +55,6 @@
             </form>';
         }   
         ?>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>
