f9bb1dcbe15431c59457e40fa7964e33c11ae48f
Saito14 <2101171@s.asojuku.ac.jp>
2022-12-09 11:18:51 +0900

    Update item.php

--- item.php
+++ item.php
@@ -36,6 +36,7 @@
               </div>
             </div>
         </nav>
+        <div class="container">
         <div class="search-bar">
         <form action="" method="POST" id="form1">
             <input class="search-box" style="height: 40px;" type="text" placeholder="🔍キーワード検索" name="keyword" id="sbox">
@@ -94,6 +95,7 @@
             </ul>
             </div>
             </form>
+            </div>
             </div>
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
